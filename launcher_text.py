819373bd677a75636b379ad522b1fd1c1ae863bc
Raphaël Nuytten <raph@MacBook-Air-de-Raphael.local>
2018-03-01 15:25:51 +0100

    Ajout du choix du nombre de nombre

--- launcher_text.py
+++ launcher_text.py
@@ -2,12 +2,11 @@ import random
 import count_error as ce
 
 class LauncherText:
-    def __init__(self, valeur_min, valeur_max):
-        self.nbr_1 = random.randint(valeur_min, valeur_max)
-        self.nbr_2 = random.randint(valeur_min, valeur_max)
+    def __init__(self):
+        pass
 
-    def check_rep(self, rep):
-        correct = self.nbr_1 + self.nbr_2
+    def check_rep(self, rep, total):
+        correct = total
         if rep == correct:
             print("Bien joué !")
             print("Réponse : ", correct)
@@ -17,23 +16,32 @@ class LauncherText:
             print("Réponse : ", correct)
             return True
 
+    def random_nbr(self, nbr):
+        total = 0
+        for nombre in range(nbr):
+            nombre = random.randint(valeur_min, valeur_max)
+            total += nombre
+            print(nombre)
+        return total
+
 if __name__ == '__main__' :
     print("Nombre de répétitions ?")
     nbr_rep = int(input())
+    print("Combien de nombre ?")
+    nbr = int(input())
     print("Valeur min ?")
     valeur_min = int(input())
     print("Valeur max ?")
     valeur_max = int(input())
     error = ce.CountError()
+    soroban = LauncherText()
+    total = 0
     for play in range(nbr_rep):
-        soroban = LauncherText(valeur_min, valeur_max)
-        print(soroban.nbr_1)
-        print(soroban.nbr_2)
+        total = soroban.random_nbr(nbr)
         rep = int(input())
-        error.is_correct(soroban.check_rep(rep))
+        error.is_correct(soroban.check_rep(rep, total))
         nbr_rep -= 1
         print("Nombre de répétitions restantes : ", nbr_rep)
     print("Nombre total d'erreur : ", error.nbr_error)
 
-#Introduire choix du nombre de Nombre
 #Introduire choix addition/soustraction/multiplication
